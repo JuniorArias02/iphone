9fa7769fc91f1888489a9330446825197b3da6b5
Junior Arias <158210736+JuniorArias02@users.noreply.github.com>
2024-05-30 17:50:11 -0500

    actualiazcion

--- resources/transicion.js
+++ resources/transicion.js
@@ -13,7 +13,6 @@ document.querySelector('.boton-encender').addEventListener('click', function() {
     }
 });
 
-
 document.querySelector('.boton-silencio').addEventListener('click', function() {
     var tonoIcon = document.querySelector('.tono-icon img');
     if (tonoIcon.src.includes('alerta-de-notificacion.png')) {
@@ -25,9 +24,9 @@ document.querySelector('.boton-silencio').addEventListener('click', function() {
 
 document.querySelector('.boton-silencio').addEventListener('click', function() {
     var tonoIcon = document.querySelector('.boton-tono');
-    tonoIcon.style.opacity = '0.5'; // Mostrar el icono
+    tonoIcon.style.opacity = '0.5'; 
     setTimeout(function() {
-        tonoIcon.style.opacity = '0'; // Ocultar el icono después de 1 segundo
+        tonoIcon.style.opacity = '0'; 
     }, 1000);
 });
 
@@ -36,19 +35,14 @@ function actualizarHora() {
     var horas = fecha.getHours();
     var ampm = horas >= 12 ? 'PM' : 'AM';
     horas = horas % 12;
-    horas = horas ? horas : 12; // Si son las 0 horas, establecer como 12
+    horas = horas ? horas : 12;
     var minutos = fecha.getMinutes();
-    // Agregar ceros delante de los minutos si es necesario
     if (minutos < 10) {
         minutos = '0' + minutos;
     }
-    // Formatear la hora en formato HH:MM AM/PM
     var horaActual = horas + ':' + minutos ;
-    // Actualizar el contenido del elemento con la hora actual
     document.getElementById('hora-actual').innerText = horaActual;
 }
-
-// Llamar a la función para actualizar la hora cada segundo
 setInterval(actualizarHora, 1000);
 
 function actualizarFecha() {
@@ -58,11 +52,8 @@ function actualizarFecha() {
     var diaSemana = dias[fecha.getDay()];
     var diaMes = fecha.getDate();
     var mes = meses[fecha.getMonth()];
-    // Formatear la fecha en el formato "Día de la semana, Mes Día, Año"
     var fechaActual = diaSemana + ', ' + mes + ' ' + diaMes;
-    // Actualizar el contenido del elemento con la fecha actual
     document.getElementById('fecha-actual').innerText = fechaActual;
 }
 
-// Llamar a la función para actualizar la fecha cada segundo
 actualizarFecha();
